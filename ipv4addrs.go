2c4de554ade0c14d1a6bd3bd93273aa0bab9f581
Sean Chittenden <sean@chittenden.org>
2016-09-25 23:56:01 -0700

    Flesh out IPv4Addrs to support more ways of sorting per sort.Interface.

--- ipv4addrs.go
+++ ipv4addrs.go
@@ -3,32 +3,32 @@ package sockaddr
 // IPv4Addrs is a collection of IPv4Addrs
 type IPv4Addrs []IPv4Addr
 
-func (s IPv4Addrs) Len() int      { return len(s) }
+// Len returns the length of the IPv4Addrs array
+func (s IPv4Addrs) Len() int { return len(s) }
+
+// Swap swaps the elements with indexes i and j.
 func (s IPv4Addrs) Swap(i, j int) { s[i], s[j] = s[j], s[i] }
 
-// Sort IPv4Addrs by smallest network/most specific to largest network
-type SortIPv4AddrsBySpecificMaskLen struct{ IPv4Addrs }
+// SortIPv4AddrsByNetwork is a type that satisfies sort.Interface and can be
+// used by the routines in this package.  The SortIPv4AddrsByNetwork type is
+// used to sort IPv4Addrs by numerical order from lowest to highest.
+type SortIPAddrsByNetwork struct{ IPv4Addrs }
 
-func (s SortIPv4AddrsBySpecificMaskLen) Less(i, j int) bool {
-	return s.IPv4Addrs[i].Maskbits() > s.IPv4Addrs[j].Maskbits()
+// Less reports whether the element with index i should sort before the
+// element with index j.
+func (s SortIPAddrsByNetwork) Less(i, j int) bool {
+	iAddr := s.IPv4Addrs[i].NetworkAddress()
+	jAddr := s.IPv4Addrs[j].NetworkAddress()
+	if iAddr != jAddr {
+		return iAddr < jAddr
+	}
+
+	// Sort smaller networks first
+	return s.IPv4Addrs[i].Mask > s.IPv4Addrs[j].Mask
 }
 
-// Sort by largest to smallest network size
-type SortIPv4AddrsByBroadMaskLen struct{ IPv4Addrs }
-
-func (s SortIPv4AddrsByBroadMaskLen) Less(i, j int) bool {
-	return s.IPv4Addrs[i].Maskbits() < s.IPv4Addrs[j].Maskbits()
-}
-
-// Sort by network address
-type SortIPv4AddrsByNetwork struct{ IPv4Addrs }
-
-func (s SortIPv4AddrsByNetwork) Less(i, j int) bool {
-	return s.IPv4Addrs[i].ToUint32() < s.IPv4Addrs[j].ToUint32()
-}
-
-// FilterByTypeIPv4Addr filters Sockaddrs and returns a list of IPv4Addrs
-func (sas Sockaddrs) FilterByTypeIPv4Addr() (ipv4addrs IPv4Addrs) {
+// FilterByTypeIPv4Addr filters SockAddrs and returns a list of IPv4Addrs
+func (sas SockAddrs) FilterByTypeIPv4Addr() (ipv4addrs IPv4Addrs) {
 	ipv4addrs = make(IPv4Addrs, 0, sas.Len())
 	for _, sa := range sas {
 		if sa.Type() == TypeIPv4 {
