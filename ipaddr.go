f6c823b04dd629f497fe6d2563836ecddc377478
Sean Chittenden <sean@chittenden.org>
2016-09-25 23:13:35 -0700

    Add NewIPAddr(string) as a helper function

--- ipaddr.go
+++ ipaddr.go
@@ -1,6 +1,9 @@
 package sockaddr
 
-import "net"
+import (
+	"fmt"
+	"net"
+)
 
 // IPAddr is a generic IP address interface for IPv4 and IPv6 addresses,
 // networks, and socket endpoints.
@@ -8,6 +11,7 @@ type IPAddr interface {
 	SockAddr
 	AddressBinString() string
 	AddressHexString() string
+	Cmp(SockAddr) int
 	CmpAddress(SockAddr) int
 	CmpPort(SockAddr) int
 	DialPacketArgs() (string, string)
@@ -20,10 +24,24 @@ type IPAddr interface {
 	ListenStreamArgs() (string, string)
 	Maskbits() int
 	NetIP() *net.IP
+	NetIPMask() *net.IPMask
 	NetIPNet() *net.IPNet
 	Network() IPAddr
-	SetPort(uint16) IPAddr
-	String() string
 }
 
 type IPPort uint16
+
+// NewIPAddr creates a new IPAddr from a string.
+func NewIPAddr(addr string) (IPAddr, error) {
+	ipv4Addr, err := NewIPv4Addr(addr)
+	if err == nil {
+		return ipv4Addr, nil
+	}
+
+	ipv6Addr, err := NewIPv6Addr(addr)
+	if err == nil {
+		return ipv6Addr, nil
+	}
+
+	return nil, fmt.Errorf("invalid IPAddr %v", addr)
+}
