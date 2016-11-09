225fdae1b06f8d7ef60b7c68894703be79441915
Sean Chittenden <sean@chittenden.org>
2016-11-08 16:29:36 -0800

    Fix up the comment re: NewSockAddr().

--- sockaddr.go
+++ sockaddr.go
@@ -43,18 +43,18 @@ type SockAddr interface {
 }
 
 // New creates a new SockAddr from the string.  The order in which New()
-// attempts to construct a SockAddr is: SockAddrUnix, IPv4Addr, IPv6Addr.
+// attempts to construct a SockAddr is: IPv4Addr, IPv6Addr, SockAddrUnix.
 //
-// NOTE: New() relies on the heuristic wherein the path begins with either a '.'
-// or '/' character before creating a new UnixSock.  For UNIX sockets that are
-// absolute paths or are nested within a sub-directory, this works as expected,
-// however if the UNIX socket is contained in the current working directory,
-// this will fail unless the path begins with "./" (e.g. "./my-local-socket").
-// Calls directly to NewUnixSock() do not suffer this limitation.  Invalid IP
-// addresses such as "256.0.0.0/-1" will run afoul of this heuristic and be
-// assumed to be a valid UNIX socket path (which they are, but it is probably
-// not what you want and you won't realize it until you stat(2) the file system
-// to discover it doesn't exist).
+// NOTE: New() relies on the heuristic wherein if the path begins with either a
+// '.'  or '/' character before creating a new UnixSock.  For UNIX sockets that
+// are absolute paths or are nested within a sub-directory, this works as
+// expected, however if the UNIX socket is contained in the current working
+// directory, this will fail unless the path begins with "./"
+// (e.g. "./my-local-socket").  Calls directly to NewUnixSock() do not suffer
+// this limitation.  Invalid IP addresses such as "256.0.0.0/-1" will run afoul
+// of this heuristic and be assumed to be a valid UNIX socket path (which they
+// are, but it is probably not what you want and you won't realize it until you
+// stat(2) the file system to discover it doesn't exist).
 func NewSockAddr(s string) (SockAddr, error) {
 	ipv4Addr, err := NewIPv4Addr(s)
 	if err == nil {
